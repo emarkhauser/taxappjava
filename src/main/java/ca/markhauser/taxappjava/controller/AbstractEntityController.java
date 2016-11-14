4b26cb4137e627dcd9594dbaed16fd51647f7358
Erik Markhauser <emarkhauser@yahoo.com>
2016-11-13 22:27:01 -0500

    Change gitignore to exclude classpath file.  Remove unused dependencies from AbstractEntityController

--- src/main/java/ca/markhauser/taxappjava/controller/AbstractEntityController.java
+++ src/main/java/ca/markhauser/taxappjava/controller/AbstractEntityController.java
@@ -10,9 +10,6 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
 import ca.markhauser.taxappjava.service.EntityService;
 
 public abstract class AbstractEntityController<T> implements EntityController<T> {
